915546a0285b886c5d63bb373e6e68efc23f623b
makhotso <thato.makhotso@gmail.com>
2026-02-19 15:11:38 +0200

    Update microservices: Learner CRUD, Assignment endpoints, Grade and Teacher services, roll number logic

--- src/main/java/com/thato/schoolmanagement/schoolmanagement/repository/GradeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.thato.schoolmanagement.schoolmanagement.repository;
-
-import com.thato.schoolmanagement.schoolmanagement.entity.Grade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GradeRepository extends JpaRepository<Grade, Long> {
-}
-
--- src/main/java/com/thato/schoolmanagement/schoolmanagement/repository/LearnerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.thato.schoolmanagement.schoolmanagement.repository;
-
-import com.thato.schoolmanagement.schoolmanagement.entity.Learner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LearnerRepository extends JpaRepository<Learner, Long> {
-}
-
