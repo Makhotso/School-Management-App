5df113413a6ef758b9ab0167629e05f073eadc6e
Makhotso <thato.makhotso@gmail.com>
2026-03-11 14:57:19 +0200

    Update in index.js, addition of bootstrap icons installation and import

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
-// src/index.js
-import React from "react";               //  import React
-import ReactDOM from "react-dom/client"; // import ReactDOM for React 18+
-import { BrowserRouter } from "react-router-dom"; //  import BrowserRouter
-import App from "./App";                 //  import your App component
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // optional for modals, dropdowns
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
