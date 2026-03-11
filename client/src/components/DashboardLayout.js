0c94c6740cd5eb0555c5010c374ab34500ddc7ba
Makhotso <thato.makhotso@gmail.com>
2026-03-11 14:54:00 +0200

    Update of Admin Dashboard Layout

--- client/src/components/DashboardLayout.js
+++ client/src/components/DashboardLayout.js
@@ -1,14 +1,55 @@
-// src/components/DashboardLayout.js
 import React from "react";
-import "../styles/DashboardLayout.css"; // <-- import from styles
+import { Link } from "react-router-dom";
+import "../styles/DashboardLayout.css";
 
 const DashboardLayout = ({ children }) => {
+
   return (
+
     <div className="dashboard-container">
-      <div className="dashboard-content">
+
+      <aside className="sidebar">
+
+        <h3>Admin Portal</h3>
+
+        <ul>
+
+          <li>
+            <Link to="/admin/dashboard">Dashboard</Link>
+          </li>
+
+          <li>
+            <Link to="/admin/learners">Learners</Link>
+          </li>
+
+          <li>
+            <Link to="/admin/teachers">Teachers</Link>
+          </li>
+
+          <li>
+            <Link to="/admin/grades">Grades</Link>
+          </li>
+
+          <li>
+            <Link to="/admin/assignments">Assignments</Link>
+          </li>
+
+          <li>
+            <Link to="/admin/announcements">Announcements</Link>
+          </li>
+
+        </ul>
+
+      </aside>
+
+      <main className="dashboard-content">
+
         {children}
-      </div>
+
+      </main>
+
     </div>
+
   );
 };
 
